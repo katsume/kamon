d53f9ac0c6a249c2ff8b8ab45a6a8e73381f3696
Yuichiro Katsume <katsume@users.noreply.github.com>
2020-02-14 21:37:00 +0900

    Scraper : Modify

--- scraper/index.js
+++ scraper/index.js
@@ -1,22 +1,28 @@
 const fs= require('fs')
 const url= require('url')
+const path= require('path')
 
-const axios= require('axios')
+const program= require('commander')
 const cheerio= require('cheerio')
+const rp= require('request-promise')
+
+program
+	.option('--base <url>', '', 'http://www.morisige.com/html/hina/tabi/japanese/kamon-hyou-top.html')
+	.option('--dst <path>', 'Save images here', process.cwd())
+	.parse(process.argv)
 
 ;(async ()=>{
 
 	const pages= []
+	const images= []
 
 	try {
-		const base= 'http://www.morisige.com/html/hina/tabi/japanese/kamon-hyou-top.html'
-		const {data}= await axios.get(base)
+		const data= await rp(program.base)
 		const $= cheerio.load(data)
 		$('a[href*="kamon"]').each((i, elm)=>{
-			const href= new URL($(elm).attr('href'), base)
+			const href= new URL($(elm).attr('href'), program.base)
 			pages.push(href.toString())
 		})
-
 	} catch(err) {
 		console.log(err)
 	}
@@ -24,10 +30,28 @@ const cheerio= require('cheerio')
 	for(let i=0; i<pages.length; i++){
 		try {
 			const page= pages[i]
-			const {data}= await axios.get(page)
+			const data= await rp(page)
 			const $= cheerio.load(data)
 			const src= new URL($('img').attr('src'), page)
-			process.stdout.write(src.toString()+'\n')
+			images.push(src.toString())
+		} catch(err) {
+			console.log(err)
+		}
+	}
+
+	if(!fs.existsSync(program.dst)){
+		fs.mkdirSync(program.dst)
+	}
+	for(let i=0; i<images.length; i++){
+		try {
+			const uri= images[i]
+			const basename= path.basename(uri)
+			const res= await rp.get({
+				uri: uri,
+				encoding: null
+			})
+			const buf= Buffer.from(res)
+			fs.writeFileSync(path.join(program.dst, basename), buf)
 		} catch(err) {
 			console.log(err)
 		}
